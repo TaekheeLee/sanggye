3aca2828baac006f72d70a6051f3033b7f19430a
TaekheeLee <thlee@suholdings.com>
2015-06-08 07:58:35 +0900

    camera

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -16,68 +16,129 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var curPage		= 1,
-	itemPerPage	= 20;
+var curPage = 1,
+    itemPerPage = 10;
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        // if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
-	        // document.addEventListener("deviceready", this.onDeviceReady, false);
-	    // } else {
-	        // this.onDeviceReady();
-	    // }
-	    
-	    //document.addEventListener("deviceready", this.onDeviceReady, false);
-	    this.onDeviceReady();
-	    
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        app.getList();
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        
-    },
-    
+	// Application Constructor
+	initialize : function() {
+		this.bindEvents();
+	},
+	// Bind Event Listeners
+	//
+	// Bind any events that are required on startup. Common events are:
+	// 'load', 'deviceready', 'offline', and 'online'.
+	bindEvents : function() {
+		this.onDeviceReady();
+
+	},
+	// deviceready Event Handler
+	//
+	// The scope of 'this' is the event. In order to call the 'receivedEvent'
+	// function, we must explicitly call 'app.receivedEvent(...);'
+	onDeviceReady : function() {
+		app.receivedEvent('deviceready');
+		app.search('');
+
+		$(document).on('pageinit', function() {
+			$("#schtxt").keydown(function(event) {
+				if (event.keyCode == 13) {
+					app.search($("#schtxt").val());
+				}
+			});
+		});
+
+	},
+	// Update DOM on a Received Event
+	receivedEvent : function(id) {
+
+	},
+
+	search : function(txt) {
+		var listColumn = ['uid', 'thumbnail', 'number', 'regno', 'owner', 'job', 'worship', 'type'];
+		var app = Built.App(common.bltappid());
+		var ParkClass = app.Class('Park');
+		var query1 = ParkClass.Query();
+		query1 = query1.matches('number', txt, 'i');
+		var query2 = ParkClass.Query();
+		query2 = query2.matches('regno', txt, 'i');
+		var query = ParkClass.Query();
+		query = query.or([query1, query2]);
+		query = query.limit(itemPerPage);
+		query = query.descending('updated_at');
+		query = query.only(listColumn);
+
+		query.exec().then(function(parks) {
+			console.log("count:" + parks.length);
+			$('#car-data').empty();
+			if(parks.length > 0){
+				$('#no-car').hide();
+				var output = '';
+				$.each(parks, function(index, value) {
+					output += "<li><a href='show.html?id=" + value.get('uid') + "' data-ajax='false'>";
+					output += "<img src='data:image/jpeg;base64," + value.get('thumbnail') + "'>";
+					output += "<h2>" + value.get('number') + "</h2>";
+					output += "<p>" + value.get('owner') + common.checkJob(value.get('job')) + "/" + common.checkWorship(value.get('worship')) + "<br>" + value.get('type') + "&nbsp;</p>";
+					output += "</a></li>";
+				});
 	
-	getList: function(){
-    	var listColumn      = ['uid','thumbnail','number','regno','owner','job','worship','type'];
-      	var lstQuery    = new Built.Query('Park');
-        lstQuery        = lstQuery.skip((curPage-1)*itemPerPage);
-        lstQuery        = lstQuery.limit(itemPerPage);
-        lstQuery        = lstQuery.descending('updated_at');
-        lstQuery        = lstQuery.only(listColumn);
-        lstQuery.exec().
-            onSuccess(function(parks) {
-            	console.log("count:" + parks.length);
-                var output = '';
-                $.each(parks, function(index, value){
-			        output += "<li><a href='show.html?id=" + value.get('uid') + "' data-ajax='false'>";
-			        output += "<img src='data:image/jpeg;base64," + value.get('thumbnail') + "'>";
-			        output += "<h2>" + value.get('number') + "</h2>";
-			        output += "<p>" + value.get('owner') + value.get('job') + "/" + value.get('worship') + "<br>" + value.get('type') + "&nbsp;</p>";
-			        output += "</a></li>";
-			    });
-			    
-			    $('#car-data').append(output).listview('refresh');
-        });
-    }
-      
+				$('#car-data').append(output).listview('refresh');
+				$('#car-data').show();
+				
+			}else{
+				$('#car-data').hide();
+				$('#no-car').show();
+			}
+		});
+
+	},
+	
+	regist: function(uid){
+		var App = Built.App(common.bltappid()).setMasterKey(common.masterid());
+		var Park = App.Class('Park').Object;
+		var park = Park(uid);
+		park = park.set('published', true);
+		
+		park.save()
+			.then(function(park){
+				console.log("saveAsDrafe->save() Ok");
+				$('#frmRegist').submit();
+			},function(error){
+				console.log("saveAsDrafe->save() Error");
+			});
+	},
+	
+	saveDraft: function(){
+		try{
+			var App = Built.App(common.bltappid()).setMasterKey(common.masterid());
+			var Park = App.Class('Park').Object();
+
+			Park  = Park.assign({
+				regno : $("#regno").val(),
+				number: $("#number").val(),
+				owner : $("#owner").val(),
+				job	  : $("#select-job").val(),
+				worship : $("#select-worship").val(),
+				telno : $("#telno").val(),
+				telno2: $("#telno2").val(),
+				type  : $("#type").val(),
+				memo  : $("#memo").val()
+			});
+			
+			Park.saveAsDraft()
+				.then(function(draft){
+					console.log("Insert:" + draft.getUid());
+					app.regist(draft.getUid());
+				}, function(error){
+					console.log("Error");
+					alert(error);
+				});
+			
+		}catch(e){
+			console.log(e);
+		}
+		
+	}
 };
 
 app.initialize();
-
-
 
--- platforms/android/assets/www/js/show.js
+++ platforms/android/assets/www/js/show.js
@@ -1,26 +1,3 @@
-var QueryString = function () {
-	// This function is anonymous, is executed immediately and 
-	// the return value is assigned to QueryString!
-	console.log("QueryString Exec," + window.location.search.substring(1) + "," + window.location.pathname + "," );
-	var query_string = {};
-	var query = window.location.search.substring(1);
-	var vars = query.split("&");
-	for (var i=0;i<vars.length;i++) {
-		var pair = vars[i].split("=");
-		// If first entry with this name
-		if (typeof query_string[pair[0]] === "undefined") {
-			query_string[pair[0]] = pair[1];
-			// If second entry with this name
-		} else if (typeof query_string[pair[0]] === "string") {
-			var arr = [ query_string[pair[0]], pair[1] ];
-			query_string[pair[0]] = arr;
-			// If third or later entry with this name
-		} else {
-			query_string[pair[0]].push(pair[1]);
-		}
-	} 
-	return query_string;
-} ();
 var app = {
     // Application Constructor
     initialize: function() {
@@ -31,12 +8,6 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        // if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
-	        // document.addEventListener("deviceready", this.onDeviceReady, false);
-	    // } else {
-	        // this.onDeviceReady();
-	    // }
-	    
 	    this.onDeviceReady();
     },
     // deviceready Event Handler
@@ -45,77 +16,122 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        console.log("uid:" + QueryString.id);
+        console.log("uid:" + common.QueryString.id);
         app.getContent();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         
     },
-    
 	
 	getContent: function(){
-		var uid 		= QueryString.id;
-    	var cntColumn  	= ['uid','thumbnail','number','regno','owner','telno','job','worship','type'];
-    	var query		= new Built.Query('Park');
+		var uid 		= common.QueryString.id;
+    	var cntColumn  	= ['uid','number','regno','owner','telno','telno2','job','worship','type','key','car','memo'];
+    	var app			= Built.App(common.bltappid());
+    	var ParkClass	= app.Class('Park');
+    	//var query		= new Built.Query('Park');
+    	var query		= ParkClass.Query();
     	query			= query.where('uid', uid);
     	query			= query.only(cntColumn);
     	
         query.exec()
         	.then(function(park){
         		console.log("Car:" + park[0].get("regno"));
-        		$('#regno').val(park[0].get("number"));
+        		//$('#regno').val(park[0].get("number"));
         		$('#car_id').html(
-        			"(" + app.nullCheck(park[0].get("regno")) + ") " + app.nullCheck(park[0].get("number"))
+        			"(" + common.nullCheck(park[0].get("regno")) + ") " + common.nullCheck(park[0].get("number"))
         		);
-        		$('#owner_job').html(park[0].get("owner") + app.checkJob(park[0].get("job")));
-        		$('#telno').html(app.telFormat(park[0].get("telno")));
-        		$('worship').html(app.checkWorship(park[0].get('worship')));
+        		$('#owner_job').html(park[0].get("owner") + common.checkJob(park[0].get("job")));
+        		$('#telno').html(common.telFormat(park[0].get("telno")));
+        		$('#worship').html(common.checkWorship(park[0].get('worship')));
+        		$('#keyImg').attr('src', 'data:image/jpeg;base64,' + park[0].get('key'));
+        		$('#popKeyImg').attr('src', 'data:image/jpeg;base64,' + park[0].get('key'));
+        		$('#carImg').attr('src', 'data:image/jpeg;base64,' + park[0].get('car'));
+        		$('#popCarImg').attr('src', 'data:image/jpeg;base64,' + park[0].get('car'));
+        		
+        		$('#more_owner_job').html(park[0].get("owner") + common.checkJob(park[0].get("job")));
+        		$('#more_telno').html($('#telno').html());
+        		$('#more_telno2').html(common.telFormat(park[0].get("telno2")));
+        		$('#more_cartype').html(park[0].get("type"));
+        		$('#more_memo').html(park[0].get("memo"));
+        		
+        		$("#e_regno").val(park[0].get("regno"));
+        		$("#e_number").val(park[0].get("number"));
+        		$("#e_owner").val(park[0].get("owner"));
+        		$("#e_select-job").val(park[0].get("job"));
+        		$("#e_select-worship").val(park[0].get("worship"));
+        		$("#e_telno").val(park[0].get("telno"));
+        		$("#e_telno2").val(park[0].get("telno2"));
+        		$("#e_type").val(park[0].get("type"));
+        		$("#e_memo").val(park[0].get("memo"));
+        		
         	}, function(error){
         		conole.log("ERROR" + error);
         	});
     },
-    
-    nullCheck: function(obj){
-    	return (typeof obj == "undefined")?"":obj;
-    },
-    
-    telFormat: function(tel){
-    	if(tel.length < 10) return tel;
-    	if(tel.indexOf("-") > 0) return tel;
-    	return tel.replace(/(\d\d\d)(\d\d\d\d)(\d\d\d\d)/, '$1-$2-$3');
-    },
-    
-    checkJob: function(job){
-    	var txt;
-    	var iJob;
-    	iJob = (this.nullCheck(job)==null)?0:parseInt(job);
-    	switch(iJob){
-    		case 1: txt ="성도님"; break;
-    		case 2: txt ="집사님"; break;
-    		case 3: txt ="권사님"; break;
-    		case 4: txt ="장로님"; break;
-    		case 5: txt ="전도사님"; break;
-    		case 6: txt ="사모님"; break;
-    		case 7: txt ="목사님"; break;
-    		default: txt="님";	
-    	}
-    	return " " + txt;
-    },
-    
-    checkWorship: function(worship){
-    	var txt;
-    	switch(worship){
-    		case "1": txt ="1부(7시)"; break;
-    		case "2": txt ="2부(8시)"; break;
-    		case "3": txt ="3부(10시)"; break;
-    		case "4": txt ="4부(12시)"; break;
-    		case "5": txt ="5부(14시30분)"; break;
-    		default: txt="";	
-    	}
-    	return txt;
-    }
-      
+   
+   delete: function(){
+   		var yn 		= confirm('삭제하시겠습니까?');
+   		var uid 	= common.QueryString.id;
+		if(yn){
+			var App = Built.App(common.bltappid());
+			var Park = App.setMasterKey(common.masterid()).Class('Park').Object;
+			var park = Park(uid);
+			
+			park.delete()
+				.then(function(){
+					console.log("delete");
+					window.location = "index.html";
+				},function(error){
+					
+				});
+		}
+  },
+  
+  update: function(){
+		try{
+			var uid = common.QueryString.id;
+			var App = Built.App(common.bltappid()).setMasterKey(common.masterid());
+			var Park = App.Class('Park').Object;
+			var park = Park(uid);
+
+			park = park.set('regno',  $("#e_regno").val());
+			park = park.set('number', $("#e_number").val());
+			park = park.set('owner',  $("#e_owner").val());
+			park = park.set('job', 	  $("#e_select-job").val());
+			park = park.set('worship', $("#e_select-worship").val());
+			park = park.set('telno',  $("#e_telno").val());
+			park = park.set('telno2', $("#e_telno2").val());
+			park = park.set('type',   $("#e_type").val());
+			park = park.set('memo',   $("#e_memo").val());
+			
+			park.save()
+				.then(function(draft){
+					console.log("Update:OK");
+					$('#frmEdit').submit();
+				}, function(error){
+					console.log("Error");
+					alert(error);
+				});
+			
+		}catch(e){
+			console.log(e);
+		}
+		
+	},
+	
+	takePic : function(){
+		navigator.camera.getPicture(app.onSuccess, app.onFail, { quality: 50, destinationType: Camera.DestinationType.DATA_URI });
+	},
+	
+	onSuccess : function(imageData){
+		$('#testImg').attr("src","data:image/jpeg;base64," + imageData);
+	},
+	
+	onFail : function(message){
+		alert('message');
+	}
+   
 };
 
 app.initialize();
